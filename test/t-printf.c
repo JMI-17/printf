ead0de2e52c5ad7afc048e0db02a0ad79f76c69b
JMI-17 <jmi17.jm@gmail.com>
2023-11-11 21:43:48 -0800

    First commit for the printf Group projectr

---
+++ test/t-printf.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+* _printf - Custom printf function.
+* @format: Format string with optional conversion specifiers.
+*
+* Return: The number of characters printed (excluding the null byte used to end output to strings).
+*/
+int _printf(const char *format, ...)
+{
+va_list args;
+va_start(args, format);
+int count = 0;
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+count += putchar(va_arg(args, int));
+break;
+case 's':
+count += printf("%s", va_arg(args, char *));
+break;
+case '%':
+count += putchar('%');
+break;
+default:
+putchar('%');
+count++;
+break;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
+}
