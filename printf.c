508737a485687786e57f521c07214c75b951d121
JMI-17 <jmi17.jm@gmail.com>
2023-11-14 05:29:13 -0800

    PRINTF P0-T16

--- printf.c
+++ printf.c
@@ -1,45 +1,45 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
 /**
-* _printf - Custom printf function.
-* @format: Format string with optional conversion specifiers.
-* Return: The number of characters printed.
+* _printf - a function that produces output according to a format.
+* @ format - to produce output
+* Returns: the number of characters printed
 */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
-va_start(args, format);
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-switch (*format)
-{
-case 'c':
-count += putchar(va_arg(args, int));
-break;
-case 's':
-count += printf("%s", va_arg(args, char *));
-break;
-case '%':
-count += putchar('%');
-break;
-default:
-putchar('%');
-count++;
-break;
-}
-}
-else
-{
-putchar(*format);
-count++;
-}
-format++;
-}
-va_end(args);
-return (count);
+	int count = 0;
+	va_list args;
+	va_start(args, format);
+	for (int i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+				    putchar(va_arg(args, int));
+				    count++;
+				    break;
+				case 's':
+				    fputs(va_arg(args, char*), stdout);
+				    count += strlen(va_arg(args, char*));
+				    break;
+				case '%':
+				    putchar('%');
+				    count++;
+				    break;
+				default:
+				    break;
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			count++;
+		}
+	}
+	va_end(args);
+	return count;
 }
