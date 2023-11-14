9448e527aafc01d77b372e04e407ed8caa53b894
JMI-17 <jmi17.jm@gmail.com>
2023-11-14 05:32:43 -0800

    PRINTF P0-T16

--- printf.c
+++ printf.c
@@ -6,40 +6,37 @@
 * @ format - to produce output
 * Returns: the number of characters printed
 */
-int _printf(const char *format, ...)
-{
-	int count = 0;
-	va_list args;
-	va_start(args, format);
-	for (int i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			switch (format[i])
-			{
-				case 'c':
-				    putchar(va_arg(args, int));
-				    count++;
-				    break;
-				case 's':
-				    fputs(va_arg(args, char*), stdout);
-				    count += strlen(va_arg(args, char*));
-				    break;
-				case '%':
-				    putchar('%');
-				    count++;
-				    break;
-				default:
-				    break;
-			}
-		}
-		else
-		{
-			putchar(format[i]);
-			count++;
-		}
-	}
-	va_end(args);
-	return count;
+int _printf(const char *format, ...) {
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+    for (int i = 0; format[i] != '\0'; i++) {
+        if (format[i] == '%') {
+            i++;
+            switch (format[i]) {
+                case 'c':
+                    putchar(va_arg(args, int));
+                    count++;
+                    break;
+                case 's':
+                    printf("%s", va_arg(args, char*));
+                    count += strlen(va_arg(args, char*));
+                    break;
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                default:
+                    putchar(format[i]);
+                    count++;
+                    break;
+            }
+        } else {
+            putchar(format[i]);
+            count++;
+        }
+    }
+    va_end(args);
+    return count;
 }
