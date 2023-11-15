2cdb76db518ec537a63d571894504922127b69c9
Lady-Bee <117857595+Lady-Bee@users.noreply.github.com>
2023-11-15 20:15:28 +0100

    Update 0jb-func1.c

--- 0jb-func1.c
+++ 0jb-func1.c
@@ -3,10 +3,10 @@
 /************************* THIS FUNCTION PRINTS UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
- * @types: List a of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width
+ * @width: gets width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed.
@@ -38,10 +38,10 @@ int print_unsigned(va_list types, char buffer[],
 /************* THIS FUNCTION PRINTS UNSIGNED NUMBER IN OCTAL  ****************/
 /**
  * print_octal - Prints an unsigned number in octal notation
- * @types: Lista of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width
+ * @width: gets width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
@@ -80,10 +80,10 @@ int print_octal(va_list types, char buffer[],
 /************** THIS FUNCTION PRINTS UNSIGNED NUMBER IN HEXADECIMAL **************/
 /**
  * print_hexadecimal - Prints an unsigned number in hexadecimal notation
- * @types: Lista of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width
+ * @width: gets width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
@@ -98,10 +98,10 @@ int print_hexadecimal(va_list types, char buffer[],
 /************* THIS FUNCTION PRINTS UNSIGNED NUMBER IN UPPER HEXADECIMAL **************/
 /**
  * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
- * @types: Lista of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width
+ * @width: gets width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
@@ -116,12 +116,12 @@ int print_hexa_upper(va_list types, char buffer[],
 /************** THIS FUNCTION PRINTS HEXX NUM IN LOWER OR UPPER **************/
 /**
  * print_hexa - Prints a hexadecimal number in lower or upper
- * @types: Lista of arguments
+ * @types: List of arguments
  * @map_to: Array of values to map the number to
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @flag_ch: Calculates active flags
- * @width: get width
+ * @width: gets width
  * @precision: Precision specification
  * @size: Size specifier
  * @size: Size specification
